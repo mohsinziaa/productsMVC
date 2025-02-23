25277f2496a2cee92bcd5ca609131d752f687099
mohsinziaa <ziamohsin8@gmail.com>
2025-02-24 04:28:22 +0500

    Add project files.

---
+++ crudmvc/Controllers/ProductsController.cs
@@ -0,0 +1,19 @@
+﻿using crudmvc.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace crudmvc.Controllers
+{
+    public class ProductsController : Controller
+    {
+        private readonly ApplicationDbContext context;
+        public ProductsController(ApplicationDbContext context)
+        {
+            this.context = context;            
+        }
+        public IActionResult Index()
+        {
+            var products = context.Products.ToList();
+            return View(products);
+        }
+    }
+}
---
+++ crudmvc/Services/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using crudmvc.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace crudmvc.Services
+{
+    public class ApplicationDbContext:DbContext
+    {
+        public ApplicationDbContext(DbContextOptions options): base(options)
+        {
+            
+        }
+
+        public DbSet<Product> Products { get; set; } 
+    }
+}
